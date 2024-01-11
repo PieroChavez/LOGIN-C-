8177ea0ddb7fda63ee445150ebc8a9bfda2763e7
Piero Chavez <you@example.com>
2024-01-10 22:26:53 -0600

    para la tarea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }
 ];
 
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
